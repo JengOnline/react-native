4d00ee1e809b21b77aae2252c51fa1ceca54487f
Jeng <jangdt20@gmail.com>
2021-04-10 17:29:57 +0700

    TabView and tab password

--- rn-starter/src/screens/TextScreen.js
+++ rn-starter/src/screens/TextScreen.js
@@ -1,19 +1,20 @@
-import React, { useReducer } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useReducer, useState } from "react";
+import { View, useWindowDimensions, Text, StyleSheet } from "react-native";
+import { TabView, SceneMap } from "react-native-tab-view";
 import { TextInput } from "react-native-gesture-handler";
 
-const reducer = (state = "", action) => {
-  switch (action.type) {
-    case "onChange": {
-      return { ...state.name, name: (state.name = action.payload) };
+const FirstRoute = () => {
+  const reducer = (state = "", action) => {
+    switch (action.type) {
+      case "onChange": {
+        return { ...state.name, name: (state.name = action.payload) };
+      }
+      default: {
+        return state;
+      }
     }
-    default: {
-      return state;
-    }
-  }
-};
+  };
 
-const TextScreen = () => {
   const [state, dispatch] = useReducer(reducer, { name: "" });
   const { name } = state;
 
@@ -23,9 +24,9 @@ const TextScreen = () => {
       <TextInput
         style={styles.textInput}
         autoCapitalize="none"
+        autoCorrect={false}
         value={name}
         onChangeText={(event) => {
-          console.log(event);
           dispatch({ type: "onChange", payload: event });
         }}
       />
@@ -34,9 +35,60 @@ const TextScreen = () => {
   );
 };
 
+const SecondRoute = () => {
+  const [password, setPassword] = useState("");
+
+  
+
+  return (
+    <View>
+      <Text style={styles.headerText}>Enter Password:</Text>
+      <TextInput
+        style={styles.textInput}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={setPassword}
+        numeric={true}
+        keyboardType={"numeric"}
+      />
+      {password.length < 5 ? (
+        <Text style={styles.textName}>
+          Password must be longer than 5 characters
+        </Text>
+      ) : null}
+    </View>
+  );
+};
+
+const TextScreen = () => {
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: "first", title: "Name" },
+    { key: "second", title: "Password" },
+  ]);
+
+  const renderScene = SceneMap({
+    first: FirstRoute,
+    second: SecondRoute,
+  });
+
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+    />
+  );
+};
+
 const styles = StyleSheet.create({
   view: {
     padding: 20,
+    flex: 1,
   },
   headerText: {
     fontSize: 30,
