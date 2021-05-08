ee889c2ffbadeff3302395ee2ee50892660b2703
Jeng <jangdt20@gmail.com>
2021-05-08 15:34:31 +0700

    change style in IndexScreen

--- Blog/src/context/createDataContext.js
+++ Blog/src/context/createDataContext.js
@@ -5,9 +5,17 @@ export default (reducer, actions, initialState) => {
 
   const Provider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
+    // actions === { addBlogPost: (dispatch) => {return ()=>{} } }
+    const boundActions = {};
+
+    for (let key in actions) {
+      boundActions[key] = actions[key](dispatch);
+    }
 
     return (
-      <Context.Provider value={{ state: state }}>{children}</Context.Provider>
+      <Context.Provider value={{ state, ...boundActions }}>
+        {children}
+      </Context.Provider>
     );
   };
 
