9b4db52807238ac99b2cacea6f30e585f20dac1c
Jeng <jangdt20@gmail.com>
2021-05-22 16:32:32 +0700

    Delete and Edit Blog post to jsonServer

--- Blog/src/context/BlogContext.js
+++ Blog/src/context/BlogContext.js
@@ -6,15 +6,6 @@ const blogReducer = (state, action) => {
   switch (action.type) {
     case "get_blogposts":
       return action.payload;
-    case "add_blogpost":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content,
-        },
-      ];
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
 
@@ -50,18 +41,23 @@ const addBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`);
+
     dispatch({ type: "delete_blogpost", payload: id });
   };
 };
 
 const editBlogPost = (dispatch) => {
-  return (id, title, content, callBack) => {
+  return async (id, title, content, callBack) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
     dispatch({
       type: "edit_blogpost",
       payload: { id, title, content },
     });
-    callBack();
+    if (callBack) {
+      callBack();
+    }
   };
 };
 
