e8e4ae1eefd1132f60b8497a4afd71dd12b7edc6
Jeng <jangdt20@gmail.com>
2021-05-22 15:55:34 +0700

    connect db

---
+++ Blog/src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://adc216165b8a.ngrok.io'
+});
--- Blog/src/context/BlogContext.js
+++ Blog/src/context/BlogContext.js
@@ -1,8 +1,11 @@
 import { useReducer } from "react";
 import createDataContext from "./createDataContext";
+import jsonServer from "../api/jsonServer";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case "get_blogposts":
+      return action.payload;
     case "add_blogpost":
       return [
         ...state,
@@ -23,6 +26,14 @@ const blogReducer = (state, action) => {
     default:
       return state;
   }
+};
+
+const getBlogPosts = (dispatch) => {
+  return async () => {
+    const response = await jsonServer.get("/blogposts");
+
+    dispatch({ type: "get_blogposts", payload: response.data });
+  };
 };
 
 const addBlogPost = (dispatch) => {
@@ -54,6 +65,7 @@ const editBlogPost = (dispatch) => {
 export const { Context, Provider } = createDataContext(
   blogReducer,
   {
+    getBlogPosts,
     addBlogPost,
     deleteBlogPost,
     editBlogPost,
