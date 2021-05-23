15841ff4870fb53a2c12ed1efbd6cfbbe329176c
Jeng <jangdt20@gmail.com>
2021-05-23 15:26:36 +0700

    router.post/signup

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,11 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("../routes/authRoutes");
 
 const app = express();
+
+app.use(authRoutes);
+app.use(bodyParserjson());
+
+
+const mongoUri =
+  "mongodb+srv://admin:passwordpassword@cluster0.cyd7c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on("error", () => {
+  console.error("Error connecting to mongo", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Hi there!");
 });
 
-app.listen(3000, ()=>{
-    console.log('Listening on port 3000')
-})
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
