91846aebf1c1fc66fc854af7d0ae6bbcc04c9001
SophonJumpapang <Sophonj@hatari.net>
2021-09-04 15:35:32 +0700

    Add CardDetailScreen

---
+++ BankApp/src/screens/Cards/CardDetailScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {StyleSheet, ImageBackground} from 'react-native';
+
+const CardDetailScreen = () => {
+  return (
+    <ImageBackground
+      source={require('../../images/BG.png')}
+      resizeMode="cover"
+      style={styles.image}></ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#000',
+  },
+});
+
+export default CardDetailScreen;
