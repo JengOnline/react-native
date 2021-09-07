3bb949896cae4770291cfbd2f7e49b00d935b3cd
SophonJumpapang <Sophonj@hatari.net>
2021-09-08 00:05:25 +0700

    add animated when scroll up

--- BankApp/src/screens/Cards/CardDetailScreen.js
+++ BankApp/src/screens/Cards/CardDetailScreen.js
@@ -1,19 +1,62 @@
-import React from 'react';
-import {View, StyleSheet, ImageBackground, SafeAreaView} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  ImageBackground,
+  SafeAreaView,
+  ScrollView,
+  Animated,
+} from 'react-native';
 import DebitCardScreen from './DebitCardScreen';
 import ActionSheetScreen from './ActionSheetScreen';
 
+HEADER_MAX_HEIGHT = 184;
+HEADER_MIN_HEIGHT = 100;
+const HEADER_HEIGHT = 100;
+
 const CardDetailScreen = () => {
+  const scrollY = new Animated.Value(0);
+  const diffClampScrollY = Animated.diffClamp(scrollY, 0, HEADER_HEIGHT);
+  const headerY = diffClampScrollY.interpolate({
+    inputRange: [0, HEADER_HEIGHT],
+    outputRange: [0, -HEADER_HEIGHT],
+  });
+  const headerHeight = scrollY.interpolate({
+    inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+    outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+    extrapolate: 'clamp',
+  });
+  const headerZindex = scrollY.interpolate({
+    inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+    outputRange: [0, 1],
+    extrapolate: 'clamp',
+  });
+
   return (
     <ImageBackground
       source={require('../../images/BG.png')}
       resizeMode="cover"
       style={styles.image}>
       <SafeAreaView style={styles.container}>
-        <View style={styles.debitCardView}>
-          <DebitCardScreen />
-        </View>
-        <ActionSheetScreen />
+        <Animated.ScrollView
+          bounces={false}
+          scrollEventThrottle={16}
+          onScroll={Animated.event(
+            [{nativeEvent: {contentOffset: {y: scrollY}}}],
+            {useNativeDriver: false},
+          )}>
+          <Animated.View
+            style={{
+              height: headerHeight,
+              zIndex: 1000,
+              elevation: 1000,
+              transform: [{translateY: headerY}],
+              left: '-7%',
+            }}>
+            <DebitCardScreen />
+          </Animated.View>
+          <ActionSheetScreen />
+        </Animated.ScrollView>
       </SafeAreaView>
     </ImageBackground>
   );
@@ -27,12 +70,12 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: 'flex-start',
     marginTop: 20,
-    flex:1
+    flex: 1,
   },
   debitCardView: {
     width: 335,
-    height: 184,
     marginLeft: '5%',
+    flex: 1,
   },
 });
 
